5806f20bd2a9bbf90b05eddba1c3023e0988ec11
Coder Kaushik <2022kucp1077@iiitkota.ac.in>
2024-08-11 22:46:23 +0530

    removed button to flip cards

--- tuf-client/src/components/FlippingCard.jsx
+++ tuf-client/src/components/FlippingCard.jsx
@@ -11,19 +11,22 @@ const FlippingCard = ({ frontText, backText }) => {
   return (
     <div className='w-96 h-96 max-xl:h-auto flex flex-col justify-center items-center gap-8'>
       <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-      {/* Front Side of the Card */}
-      <div  
-        className="w-96 h-96 max-sm:w-80 max-sm:h-80 bg-gray-100 rounded-lg flex items-center justify-center shadow-md cursor-pointer">
-        <h2 className="text-xl font-semibold text-gray-800">{frontText}</h2>
-      </div>
+        {/* Front Side of the Card */}
+        <div  
+          className="w-96 h-96 max-sm:w-80 max-sm:h-80 bg-gray-100 rounded-lg flex items-center justify-center shadow-md cursor-pointer"
+          onClick={handleFlip}
+        >
+          <h2 className="text-xl font-semibold text-gray-800">{frontText}</h2>
+        </div>
 
-      {/* Back Side of the Card */}
-      <div  
-        className="w-96 h-96 max-sm:w-80 max-sm:h-80 bg-blue-500 rounded-lg flex items-center justify-center shadow-md cursor-pointer">
-        <h2 className="text-xl font-semibold text-white">{backText}</h2>
-      </div>
-    </ReactCardFlip>
-    <button className='w-96 max-sm:w-80 p-4 bg-blue-500 hover:bg-blue-600 hover:cursor-pointer text-center text-white rounded-xl' onClick={handleFlip}>Flip The Card</button>
+        {/* Back Side of the Card */}
+        <div  
+          className="w-96 h-96 max-sm:w-80 max-sm:h-80 bg-blue-500 rounded-lg flex items-center justify-center shadow-md cursor-pointer"
+          onClick={handleFlip}
+        >
+          <h2 className="text-xl font-semibold text-white">{backText}</h2>
+        </div>
+      </ReactCardFlip>
     </div>
   );
 };
