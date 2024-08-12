7c08dfc88f15814fecf4d733aee2d2c6aa313139
Coder Kaushik <2022kucp1077@iiitkota.ac.in>
2024-08-12 11:12:47 +0530

    added useEffect to admin panel

--- tuf-admin/src/pages/AdminPanel.jsx
+++ tuf-admin/src/pages/AdminPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardForm from '../components/CardForm';
 import CardList from '../components/CardList';
 
